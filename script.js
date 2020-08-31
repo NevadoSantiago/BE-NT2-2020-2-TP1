b61d57999cea465d3c0ae5a900923e5cccfefcbd
Santiago <nevadosantiago97@gmail.com>
2020-08-31 20:54:58 -0300

    ejercicio

--- script.js
+++ script.js
@@ -9,7 +9,37 @@ const classNames = {
 const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
+const alerta = document.getElementById('alerta')
 
-function addTodo() {
-  alert('Boton Add TODO clickeado!')
+var listaTareas = []
+
+
+
+function addTodo(nombre) {
+  if(checkearNombreValido(nombre)){
+    itemCountSpan.innerHTML = parseInt(itemCountSpan.innerText,10) + 1
+    uncheckedCountSpan.innerHTML = parseInt(uncheckedCountSpan.innerText,10) + 1
+    listaTareas.push("<li><input type='checkbox' onchange='changeCheck(this)'>"+nombre+"</li>")
+    list.innerHTML = listaTareas
+  }
+
+  
+}
+function changeCheck(event){
+  if(event.checked){
+    uncheckedCountSpan.innerHTML = parseInt(uncheckedCountSpan.innerText,10) -1
+  }else{
+    uncheckedCountSpan.innerHTML = parseInt(uncheckedCountSpan.innerText,10) +1
+  }
+}
+function checkearNombreValido(nombre){
+  if(nombre.length==0){
+    alerta.innerHTML = "Debe agregar el nombre"
+    alerta.className = "error"
+    return false
+  }else{
+    alerta.innerHTML = ""
+    alerta.className = "correcto"
+    return true
+  }
 }
